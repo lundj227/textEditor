99e8d1423011a4fba2907e7e8784622e8a1d6bb6
Jack Lund <jacklund@Jacks-MacBook-Pro.local>
2024-01-11 18:12:56 -0800

    some comments and install fetaure added

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+
 
 module.exports = () => {
   return {
@@ -17,6 +16,7 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+// Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html'
@@ -43,7 +43,7 @@ module.exports = () => {
       }),
       
     ],
-
+// Add CSS loaders and babel to webpack.
     module: {
       rules: [
         {
